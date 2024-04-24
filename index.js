2aaf7a1be50d37c23e4a5a456a0c808fb2aabd10
Angela Tolwani <angela@tolwani.com>
2024-04-24 14:16:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+console.log('test')
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(num1, num2, multiply)
+
+const random = Math.ceil(Math.random() * 10);
+console.log(random);
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+console.log(mod);
+
+const testArr = [2, 12, 20, 5, 6, 8];
+const max = Math.max(...testArr);
+console.log(max)
